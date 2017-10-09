1cd25acf0e8757250600fe58e9cb8906f462cca5
shurrik <darkfores123@gmail.com>
2017-10-09 15:38:51 +0800

    'grid'

--- basebootcodegenwpf/MainWindow.xaml.cs
+++ basebootcodegenwpf/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +11,95 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using basebootcodegenwpf.Model;
+using System.Collections.ObjectModel;
 
 namespace basebootcodegenwpf
 {
+
+    public class GridItem
+    {
+        public String name { get; set; }
+        public String protoType { get; set; }
+        public String lenghth { get; set; }
+        public String isPk { get; set; }
+        public String allowNull { get; set; }
+        public String remark { get; set; }
+    }
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable tb;
+
+        /// <summary>
+        /// 下拉框数据源
+        /// </summary>        
+        public ObservableCollection<int> SelectionList
+        {
+            get { return _selectionList; }
+            set { _selectionList = value; }
+        }
+        private ObservableCollection<int> _selectionList = new ObservableCollection<int>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            SelectionList.Add(1);
+            SelectionList.Add(2);
+            SelectionList.Add(3);
+
+            Banding();
+
+
+        }
+
+        
+        private void Banding()
+        {
+            tb = new DataTable();
+            DataColumn Checked = new DataColumn("Checked");
+            DataColumn Name = new DataColumn("Name");
+            DataColumn Sex = new DataColumn("Sex");
+            DataColumn Age = new DataColumn("Age");
+            DataColumn Selection = new DataColumn("Selection", typeof(ObservableCollection<int>));
+            tb.Columns.Add(Checked);
+            tb.Columns.Add(Name);
+            tb.Columns.Add(Sex);
+            tb.Columns.Add(Age);
+            tb.Columns.Add(Selection); //如果绑定数据不采用依赖属性绑定 采用数据模板添加 Combox 则该下拉框的数据源为Selection 
+            for (int i = 0; i < 5; i++)
+            {
+                DataRow row = tb.NewRow();
+                row["Name"] = "AA" + i.ToString();
+                if (i % 2 == 0)
+                {
+                    row["Checked"] = true;
+                    row["Sex"] = "男";
+                }
+                else
+                {
+                    row["Checked"] = false;
+                    row["Sex"] = "女";
+                }
+                row["Age"] = i.ToString();
+                row["Selection"] = SelectionList;
+                tb.Rows.Add(row);
+            }
+            //如果数据源绑定要用DataContent上下文数据绑定，则xmal界面必须指定ItemSource="{Binding}"
+            dg_Grid.DataContext = tb;
+            
+        }
+
+        private void AddData_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow dr = tb.NewRow();
+            for (int columIndex = 0; columIndex < tb.Columns.Count; columIndex++)
+                dr[columIndex] = "New Row - " + columIndex.ToString();
+            tb.Rows.Add(dr);
         }
     }
 }
---
+++ basebootcodegenwpf/TableControl.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnityApp.Unity
+{
+    /// <summary>
+    /// TableControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class TableControl : UserControl
+    {
+        private DataTable _dt = new DataTable();
+        public TableControl()
+        {
+            InitializeComponent();
+            _dt.Columns.Add(new DataColumn("ParamKey", typeof(string)));
+            _dt.Columns.Add(new DataColumn("ParamValue", typeof(string)));
+            this.dgData.ItemsSource = null;
+            this.dgData.ItemsSource = _dt.DefaultView;
+        }
+
+
+        #region 自定义依赖项属性
+
+        /// <summary>
+        /// 数据源
+        /// </summary>
+        public DataTable DataSource
+        {
+            get { return ((DataView)this.dgData.ItemsSource).Table; }
+            set { SetValue(DataSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataSourceProperty =
+            DependencyProperty.Register("DataSource", typeof(DataTable), typeof(TableControl), new PropertyMetadata(new DataTable(), DataSourceChanged));
+
+
+        private static void DataSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TableControl control = d as TableControl;
+            if (e.NewValue != e.OldValue)
+            {
+                DataTable dt = e.NewValue as DataTable;
+                control._dt = dt;
+                control.dgData.ItemsSource = null;
+                control.dgData.ItemsSource = control._dt.DefaultView;
+            }
+        }
+
+        /// <summary>
+        /// 是否编辑状态
+        /// </summary>
+        public bool IsEdit
+        {
+            get { return (bool)GetValue(IsEditProperty); }
+            set { SetValue(IsEditProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsEdit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsEditProperty =
+            DependencyProperty.Register("IsEdit", typeof(bool), typeof(TableControl), new PropertyMetadata(true, IsEditChanged));
+
+        private static void IsEditChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TableControl control = d as TableControl;
+            bool isEdit = Convert.ToBoolean(e.NewValue);
+            if (!isEdit)
+            {
+                int len = control.dgData.Columns.Count;
+                control.dgData.Columns[len - 1].Visibility = Visibility.Collapsed;
+                control.dgData.IsReadOnly = true;
+                control.btnAdd.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 删除行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            ((DataRowView)this.dgData.SelectedItem).Row.Delete();
+            _dt.Rows.Remove(((DataRowView)this.dgData.SelectedItem).Row);
+        }
+
+        /// <summary>
+        /// 添加行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _dt = ((DataView)this.dgData.ItemsSource).Table;
+            DataRow dr = _dt.NewRow();
+            _dt.Rows.Add(dr);
+            this.dgData.ItemsSource = _dt.DefaultView;
+        }
+
+    }
+}
