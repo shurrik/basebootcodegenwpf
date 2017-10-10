a4aaf53e503f20aa6bedc025029fafea31b8df45
shurrik <darkfores123@gmail.com>
2017-10-10 10:35:19 +0800

    '1010'

--- basebootcodegenwpf/MainWindow.xaml.cs
+++ basebootcodegenwpf/MainWindow.xaml.cs
@@ -37,58 +37,60 @@ namespace basebootcodegenwpf
         /// <summary>
         /// 下拉框数据源
         /// </summary>        
-        public ObservableCollection<int> SelectionList
+        public ObservableCollection<String> SelectionList
         {
             get { return _selectionList; }
             set { _selectionList = value; }
         }
-        private ObservableCollection<int> _selectionList = new ObservableCollection<int>();
+        private ObservableCollection<String> _selectionList = new ObservableCollection<String>();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            SelectionList.Add(1);
-            SelectionList.Add(2);
-            SelectionList.Add(3);
+            SelectionList.Add("String");
+            SelectionList.Add("Date");
+            SelectionList.Add("Integer");
 
             Banding();
-
-
         }
 
         
         private void Banding()
         {
             tb = new DataTable();
-            DataColumn Checked = new DataColumn("Checked");
-            DataColumn Name = new DataColumn("Name");
-            DataColumn Sex = new DataColumn("Sex");
-            DataColumn Age = new DataColumn("Age");
-            DataColumn Selection = new DataColumn("Selection", typeof(ObservableCollection<int>));
-            tb.Columns.Add(Checked);
-            tb.Columns.Add(Name);
-            tb.Columns.Add(Sex);
-            tb.Columns.Add(Age);
-            tb.Columns.Add(Selection); //如果绑定数据不采用依赖属性绑定 采用数据模板添加 Combox 则该下拉框的数据源为Selection 
-            for (int i = 0; i < 5; i++)
-            {
-                DataRow row = tb.NewRow();
-                row["Name"] = "AA" + i.ToString();
-                if (i % 2 == 0)
-                {
-                    row["Checked"] = true;
-                    row["Sex"] = "男";
-                }
-                else
-                {
-                    row["Checked"] = false;
-                    row["Sex"] = "女";
-                }
-                row["Age"] = i.ToString();
-                row["Selection"] = SelectionList;
-                tb.Rows.Add(row);
-            }
+            DataColumn name = new DataColumn("name");
+            DataColumn protoType = new DataColumn("protoType", typeof(ObservableCollection<String>)); ;
+            DataColumn lenghth = new DataColumn("lenghth");
+            DataColumn isPk = new DataColumn("isPk");
+            DataColumn allowNull = new DataColumn("allowNull");
+            DataColumn remark = new DataColumn("remark");
+            //DataColumn Selection = new DataColumn("Selection", typeof(ObservableCollection<int>));
+            tb.Columns.Add(name);
+            tb.Columns.Add(protoType);
+            tb.Columns.Add(lenghth);
+            //tb.Columns.Add(isPk);
+            tb.Columns.Add(allowNull);
+            tb.Columns.Add(remark);
+            //tb.Columns.Add(Selection); //如果绑定数据不采用依赖属性绑定 采用数据模板添加 Combox 则该下拉框的数据源为Selection 
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    DataRow row = tb.NewRow();
+            //    row["name"] = "AA" + i.ToString();
+            //    if (i % 2 == 0)
+            //    {
+            //        row["allowNull"] = true;
+            //        //row["protoType"] = "String";
+            //    }
+            //    else
+            //    {
+            //        row["allowNull"] = false;
+            //        //row["protoType"] = "Date";
+            //    }
+            //    row["lenghth"] = i.ToString();
+            //    row["protoType"] = SelectionList;
+            //    tb.Rows.Add(row);
+            //}
             //如果数据源绑定要用DataContent上下文数据绑定，则xmal界面必须指定ItemSource="{Binding}"
             dg_Grid.DataContext = tb;
             
@@ -96,10 +98,11 @@ namespace basebootcodegenwpf
 
         private void AddData_Click(object sender, RoutedEventArgs e)
         {
-            DataRow dr = tb.NewRow();
-            for (int columIndex = 0; columIndex < tb.Columns.Count; columIndex++)
-                dr[columIndex] = "New Row - " + columIndex.ToString();
-            tb.Rows.Add(dr);
+            DataRow row = tb.NewRow();
+
+            row["protoType"] = SelectionList;
+            row["allowNull"] = false;
+            tb.Rows.Add(row); ;
         }
     }
 }
