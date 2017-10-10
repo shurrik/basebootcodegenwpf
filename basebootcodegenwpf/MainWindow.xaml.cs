d74389c0520608e03883aeef01bb33b41264fdc1
shurrik <darkfores123@gmail.com>
2017-10-10 14:16:54 +0800

    '1010'

--- basebootcodegenwpf/MainWindow.xaml.cs
+++ basebootcodegenwpf/MainWindow.xaml.cs
@@ -70,33 +70,12 @@ namespace basebootcodegenwpf
             DataColumn isPk = new DataColumn("isPk");
             DataColumn allowNull = new DataColumn("allowNull");
             DataColumn remark = new DataColumn("remark");
-            //DataColumn Selection = new DataColumn("Selection", typeof(ObservableCollection<int>));
             tb.Columns.Add(name);
             tb.Columns.Add(protoType);
             tb.Columns.Add(lenghth);
             //tb.Columns.Add(isPk);
             tb.Columns.Add(allowNull);
             tb.Columns.Add(remark);
-            //tb.Columns.Add(Selection); //如果绑定数据不采用依赖属性绑定 采用数据模板添加 Combox 则该下拉框的数据源为Selection 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    DataRow row = tb.NewRow();
-            //    row["name"] = "AA" + i.ToString();
-            //    if (i % 2 == 0)
-            //    {
-            //        row["allowNull"] = true;
-            //        //row["protoType"] = "String";
-            //    }
-            //    else
-            //    {
-            //        row["allowNull"] = false;
-            //        //row["protoType"] = "Date";
-            //    }
-            //    row["lenghth"] = i.ToString();
-            //    row["protoType"] = SelectionList;
-            //    tb.Rows.Add(row);
-            //}
-            //如果数据源绑定要用DataContent上下文数据绑定，则xmal界面必须指定ItemSource="{Binding}"
             dg_Grid.DataContext = tb;
             
         }
@@ -131,6 +110,11 @@ namespace basebootcodegenwpf
             else {
                 MessageBox.Show("请选择属性！");
             }
+        }
+
+        private void GenJson_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
